41d4177fafca5707a2138038cd7d3d0ad23a2e6b
hanan-a <hanan.avramovich@ex.co>
2024-02-21 10:35:32 +0200

    feat(fish): added npm functions

---
+++ .config/fish/functions/_c_npm.fish
@@ -0,0 +1,17 @@
+function _c_npm
+end
+
+function ni
+    echo "Installing npm packages..."
+    npm install
+end
+
+function nif
+    echo "Force installing npm packages..."
+    npm install --force
+end
+
+function ncheck
+    echo "Lint & Test npm..."
+    npm run lint && npm run test
+end
--- .config/fish/functions/custom_functions.fish
+++ .config/fish/functions/custom_functions.fish
@@ -3,5 +3,6 @@ function custom_functions
     _c_git
     _c_nvim
     _c_tmux
+    _c_npm
 end
 
