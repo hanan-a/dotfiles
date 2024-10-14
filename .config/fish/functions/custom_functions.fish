ca0f82043b6d8556890bde59f536203f3f704431
hanan-a <hanan.avramovich@ex.co>
2024-10-14 10:36:40 +0300

    lint(fish): indentation

--- .config/fish/functions/custom_functions.fish
+++ .config/fish/functions/custom_functions.fish
@@ -1,9 +1,9 @@
 function custom_functions
-    _c_shell
-    _c_git
-    _c_nvim
-    _c_tmux
-    _c_npm
-    _c_settings
+  _c_shell
+  _c_git
+  _c_nvim
+  _c_tmux
+  _c_npm
+  _c_settings
 end
 
