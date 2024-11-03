d36e8f6ba7a49c2c6211e2ec8e9ce23a88a10daa
hanan-a <hanan.avramovich@ex.co>
2024-11-03 11:31:37 +0200

    feat(fish): set nvim as EDITOR

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ export NVM_DIR="$HOME/.nvm"
   [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
   [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
+export EDITOR="nvim"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
