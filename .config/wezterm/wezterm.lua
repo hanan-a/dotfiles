f52167421a4373322c4fb69cf455a533d7c14210
hanan-a <hanan.avramovich@ex.co>
2024-11-07 09:02:02 +0200

    feat(wezterm): `max_fps=120`

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,14 +7,14 @@ return {
 	enable_tab_bar = false,
   -- font = wezterm.font("FiraCode Nerd Font"),
   font = wezterm.font_with_fallback({
-    -- { family = "JetBrainsMono Nerd Font", scale = 0.75 },
     { family = "JetBrainsMono Nerd Font", scale = 0.75 },
     { family = "FiraCode Nerd Font", scale = 0.75 },
   }),
   font_size = 16.0,
+  max_fps = 120,
 	-- macos_window_background_blur = 40,
 	-- macos_window_background_blur = 30,
-	
+
 	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
 	-- window_background_image_hsb = {
 	-- 	brightness = 0.01,
