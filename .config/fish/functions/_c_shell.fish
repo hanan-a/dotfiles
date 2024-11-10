fc9cb51f1a50d01a5c53331fc52d8cb8f7742fb5
hanan-a <hanan.avramovich@ex.co>
2024-11-10 10:37:26 +0200

    feat(fish): `y` for Yazi

--- .config/fish/functions/_c_shell.fish
+++ .config/fish/functions/_c_shell.fish
@@ -58,3 +58,11 @@ function ghd --description 'GH Dash - "gh dash"' --wraps "gh dash"
     gh dash
 end
 
+function y --description 'Yazi' --wraps "yazi"
+  set tmp (mktemp -t "yazi-cwd.XXXXXX")
+  yazi $argv --cwd-file="$tmp"
+  if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+    builtin cd -- "$cwd"
+  end
+  rm -f -- "$tmp"
+end
